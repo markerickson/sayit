783c3dc8a764c92205f432b57fcbf3f039552e36
Shaon Diwakar <shaon@newsmaven.co>
2013-09-29 10:15:19 -0700

    Returning fake-data from the ^/location$ method - moving everything else into the ^/story$ method.

--- backend/app.js
+++ backend/app.js
@@ -55,29 +55,39 @@ app.get('^/$', function(req, res) {
   res.send(200, "Hello");
 });
 
-app.get('^/test', function(req, res) {
+app.get('^/location', function(req, res) {
+  var fakeResults = [ "http://farm7.staticflickr.com/6164/6245876581_7db0d9a30e.jpg", "http://farm7.staticflickr.com/6095/6245904145_463da11ef8.jpg", "http://farm7.staticflickr.com/6163/6245954433_3228e748d4.jpg", "http://farm7.staticflickr.com/6100/6246479090_09f3577081.jpg", "http://farm7.staticflickr.com/6165/6245958185_658d18a3cb.jpg", "http://farm7.staticflickr.com/6105/6245938297_faab1fe857.jpg", "http://farm7.staticflickr.com/6227/6245939029_d564f0d908.jpg", "http://farm7.staticflickr.com/6052/6245906261_5ab43fcbf6.jpg", "http://farm7.staticflickr.com/6164/6246383386_235c50b96c.jpg", "http://farm7.staticflickr.com/6225/6249245508_25daf00a48.jpg" ];
 
+  res.json(fakeResults);
 });
 
-app.get('^/location$', function(req, res) {
+app.get('^/story$', function(req, res) {
 
   if (req.query.lat && req.query.long) {
-    new yql.exec("select * from flickr.photos.search where text=\"*\" and api_key=@api_key and lat=@lat and lon=@lon", function(response) {
+    new yql.exec("select * from flickr.photos.search where text=\"*\" and api_key=@api_key and lat=@lat and lon=@lon and radius=\"30\"", function(response) {
+      var story = {};
       var images = [];
       var results = response.query.results;
 
-      if (response.query.results !== null) {
-        response.query.results.photo.forEach(function (item) {
-          var url = 'http://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '.jpg'; 
-          images.push(url);
-        });
+      if (response.error) {
+        res.json(500, { "error": "There was an error" });
+      } else {
+        if (response.query.results !== null) {
+          // If there are actual results
+          response.query.results.photo.forEach(function (item) {
+            var url = 'http://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '.jpg'; 
+            images.push(url);
+          });
+          
+        }
       }
 
-      res.json(images);
+      res.json(story);
 
     }, { "api_key": api.flickrApiKey, "lat": req.query.lat, "lon": req.query.long });
   } else {
-    res.json({});
+    // Query strings do not have lat/long
+    res.json(500, { "error": "There was an error" });
   }
 
 });
